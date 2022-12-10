f4dcc56345aa5e5b1563d0604fcb348c5e24c3e7
Emilly <emillykvitoria@gmail.com>
2022-12-09 22:27:30 -0300

    Adicionando Windows Forms

---
+++ 4 - WINDOWS FORMS/WindowsFormsApp4/Teste.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp4
+{
+    public class Teste
+    {
+        public int Id { get; set; }
+        public DateTime DataHora { get; set; }
+        public string Texto { get; set; }
+        public decimal Numero { get; set; }
+    }
+}
